de6fa0485233ecf2399770a6e0b42ab54fb2f676
tray <onesimus.githua@student.moringaschool.com>
2023-11-23 17:30:53 +0300

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    evens = [num for num in num_list if num % 2 == 0]
+    evens=[num for num in num_list if num % 2 == 0]
     return evens
+
+
 
 def make_exclamation(sentence_list):
     exclamations = [sentence + '!' for sentence in sentence_list]
